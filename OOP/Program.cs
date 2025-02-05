a5b07d673a0ae1671c927f602035371561680088
Sinitsa <sinitsammi@gmail.com>
2025-02-05 17:12:05 +0300

    Program

---
+++ OOP/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int id = 1;
+            string name = "Sinitsa";
+            string description = "Student";
+
+            Player player = new Player(id, name, description);
+            player.ShowInformation();
+        }
+    }
+
+    class Player
+    {
+        private int id;
+        private string name;
+        private string description;
+
+        public Player(int id, string name, string description)
+        {
+            this.id = id;
+            this.name = name;
+            this.description = description;
+        }
+
+        public void ShowInformation()
+        {
+            Console.WriteLine( " ID: " + id);
+            Console.WriteLine(" NAME: " + name);
+            Console.WriteLine(" DESCRIPTION: " + description);
+        }
+    }
+}
