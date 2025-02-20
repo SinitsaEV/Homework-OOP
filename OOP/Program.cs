84defa6c65ce95f8d18c3a40ed4b0e18531477e9
SinitsaEV <sinitsammi@gmail.com>
2025-02-20 15:18:36 +0300

    Update Program.cs

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using System.Text;
+using OOP;
 
 namespace OOP
 {
@@ -10,9 +12,10 @@ namespace OOP
         {
             const string AddCommand = "1";
             const string RemoveCommand = "2";
-            const string СhangeBanCommand = "3";
-            const string ShowCommand = "4";
-            const string ExitCommand = "5";
+            const string BanCommand = "3";
+            const string DisbanCommand = "4";
+            const string ShowCommand = "5";
+            const string ExitCommand = "6";
 
             Console.OutputEncoding = Encoding.Unicode;
             Console.InputEncoding = Encoding.Unicode;
@@ -25,7 +28,8 @@ namespace OOP
             {
                 Console.WriteLine($"{AddCommand} - добавить игрока\n" +
                     $"{RemoveCommand} - удалить игрока\n" +
-                    $"{СhangeBanCommand} - разбанить|забанить игрока\n" +
+                    $"{BanCommand} - забанить игрока\n" +
+                    $"{DisbanCommand} - разбанить игрока\n" +
                     $"{ShowCommand} - показать игроков\n" +
                     $"{ExitCommand} - выйти");
                 Console.WriteLine("Введите команду:");
@@ -39,23 +43,15 @@ namespace OOP
                         break;
 
                     case RemoveCommand:
-                        Console.Write("Введите ID игрока для удаления: ");
-
-                        if (int.TryParse(Console.ReadLine(), out int removeId))
-                            database.RemovePlayer(removeId);
-                        else
-                            Console.WriteLine("Неверный ввод.");
-
+                        database.RemovePlayer();
                         break;
-                        
-                    case СhangeBanCommand:
-                        Console.Write("Введите ID игрока: ");
 
-                        if (int.TryParse(Console.ReadLine(), out int changeBanId))
-                            database.RemovePlayer(changeBanId);
-                        else
-                            Console.WriteLine("Неверный ввод.");
+                    case BanCommand:
+                        database.BanPlayer();
+                        break;
 
+                    case DisbanCommand:
+                        database.DisbanPlayer();                        
                         break;
 
                     case ShowCommand:
@@ -76,32 +72,129 @@ namespace OOP
     }
 
     class Player
-    {
-        private int _id;
-        private string _nickname;
-        private int _level;
-        private bool _isBanned;
-
+    {        
         public Player(int id, string nickname, int level, bool isBanned)
         {
-            _id = id;
-            _nickname = nickname;
-            _level = level;
-            _isBanned = isBanned;
+            Id = id;
+            Nickname = nickname;
+            Level = level;
+            IsBanned = isBanned;
         }
 
-        public int Id { set { _id = value; }  get { return _id; } }
-        public string Nickname { set { _nickname = value; } get { return _nickname; } }
-        public int Level { set { _level = value; } get { return _level; } }
-        public bool IsBanned { set { _isBanned = value; } get { return _isBanned; } }
+        public int Id { set; get; }
+        public string Nickname { set; get; }
+        public int Level { set; get; }
+        public bool IsBanned { set; get; }
+               
+        public void ShowInformation()
+        {
+            Console.WriteLine(" ID: " + Id);
+            Console.WriteLine(" NICKNAME: " + Nickname);
+            Console.WriteLine(" LEVEL: " + Level);
+            Console.WriteLine(" BAN: " + IsBanned);
+        }
+    }
 
-        public static Player CreatePlayer(int id)
+    class Database
+    {
+        private List<Player> _players;
+
+        public Database()
+        {
+            _players = new List<Player>();
+        }               
+
+        public void BanPlayer()
+        {
+            int id = ReadPlayerId();
+            Player player = GetPlayerById(id);
+
+            if (player != null)
+                player.IsBanned = true;
+            else
+                Console.WriteLine("Неверный ID.");
+        }
+
+        public void DisbanPlayer()
+        {
+            int id = ReadPlayerId();
+            Player player = GetPlayerById(id);
+
+            if (player != null)
+                player.IsBanned = false;
+            else
+                Console.WriteLine("Неверный ID.");
+        }
+
+        public void AddPlayer()
+        {
+            int id = GetNewPlayerId();
+            Player player = CreatePlayer(id);
+            _players.Add(player);
+        }
+
+        public void RemovePlayer()
+        {
+            int id = ReadPlayerId();
+            Player player = GetPlayerById(id);
+
+            if (player != null)
+                _players.Remove(player);
+            else
+                Console.WriteLine("Неверный ID.");
+        }
+
+        public void ShowInfo()
+        {
+            foreach (Player player in _players)
+            {
+                player.ShowInformation();
+            }
+        }
+
+        private int ReadPlayerId()
+        {
+            int id = 0;
+            bool isCorrectInput = false;
+
+            while (isCorrectInput == false)
+            {
+                Console.Write("Введите ID игрока: ");
+                
+                if (int.TryParse(Console.ReadLine(), out id) && id >= 0)
+                    isCorrectInput = true;
+                else
+                    Console.WriteLine("Некорректный ввод.");
+            }
+
+            return id;
+        }
+
+        private int GetNewPlayerId()
+        {
+            int id = 0;
+            bool isIdCorrect = false;
+
+            while (isIdCorrect == false)
+            {
+                id = ReadPlayerId();
+
+                if (IsIdExists(id) == false)
+                    isIdCorrect = true;                
+                else
+                    Console.WriteLine("Неверный ввод.");                
+            }
+
+            return id;
+        }
+
+        private Player CreatePlayer(int id)
         {
             Console.WriteLine("Введите Nickname игрока");
             string nickname = Console.ReadLine();
             int level = 0;
             bool isLevelCorrect = false;
-            bool isBanned = true;
+            bool isBanned = false;
 
             while (isLevelCorrect == false)
             {
@@ -121,56 +214,7 @@ namespace OOP
             return new Player(id, nickname, level, isBanned);
         }
 
-        public void ShowInformation()
-        {
-            Console.WriteLine(" ID: " + Id);
-            Console.WriteLine(" NICKNAME: " + Nickname);
-            Console.WriteLine(" LEVEL: " + Level);
-            Console.WriteLine(" BAN: " + IsBanned);
-        }
-    }
-
-    class Database
-    {
-        private List<Player> _players;
-
-        public Database ()
-        {
-            _players = new List<Player>();
-        }
-        
-        private int GetNewPlayerId()
-        {
-            int id = 0;
-            bool isIdCorrect = false;
-
-            while (isIdCorrect == false)
-            {
-                Console.Write("Введите ID игрока:");
-                string userInput = Console.ReadLine();
-
-                if (int.TryParse(userInput, out id) && id > 0)
-                {                    
-                    if(CheckIdExists(id) == false)
-                    {
-                        isIdCorrect = true;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Неверный ввод.");
-                        continue;
-                    }                    
-                }
-                else
-                {
-                    Console.WriteLine("Неверный ввод.");
-                }
-            }
-
-            return id;
-        }
-
-        private bool CheckIdExists(int id)
+        private bool IsIdExists(int id)
         {
             bool isIdExists = false;
 
@@ -186,7 +230,7 @@ namespace OOP
             return isIdExists;
         }
 
-        public Player GetPlayerById(int id)
+        private Player GetPlayerById(int id)
         {
             foreach (Player player in _players)
             {
@@ -198,40 +242,5 @@ namespace OOP
 
             return null;
         }        
-
-        public void СhangePlayerBan(int id)
-        {
-            Player player = GetPlayerById(id);
-
-            if (player != null)
-                player.IsBanned = !player.IsBanned;
-            else
-                Console.WriteLine("Неверный ID.");
-        }
-
-        public void AddPlayer()
-        {
-            int id = GetNewPlayerId();
-            Player player = Player.CreatePlayer(id);
-            _players.Add(player);
-        }
-
-        public void RemovePlayer(int id)
-        {
-            Player player = GetPlayerById(id);
-
-            if (player != null)
-                _players.Remove(player);
-            else
-                Console.WriteLine("Неверный ID.");
-        }
-
-        public void ShowInfo()
-        {
-            foreach(Player player in _players)
-            {
-                player.ShowInformation();
-            }
-        }
     }
 }
