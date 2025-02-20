c7e7968d7cad338c876486b89bdfd5c3b5392697
Sinitsa <sinitsammi@gmail.com>
2025-02-20 14:02:09 +0300

    Program

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -8,228 +8,198 @@ namespace OOP
     {
         static void Main(string[] args)
         {
-            const string AddCommand = "1";
-            const string RemoveCommand = "2";
-            const string СhangeBanCommand = "3";
-            const string ShowCommand = "4";
-            const string ExitCommand = "5";
-
+            Player player = new Player("Евгений");
+            PokerDeckCreator creator = new PokerDeckCreator();
+            Deck pokerDeck = creator.Create();
+            Dealer dealer = new Dealer(pokerDeck, player);
             Console.OutputEncoding = Encoding.Unicode;
-            Console.InputEncoding = Encoding.Unicode;
+            dealer.StartGame();
+        }
 
-            Database database = new Database();
+        abstract class DeckCreator
+        {
+            public abstract Deck Create();
+        }
 
-            bool isActive = true;
+        class PokerDeckCreator : DeckCreator 
+        {
+            public override Deck Create()
+            {   
+                List<Card> cards = new List<Card>();
 
-            while (isActive)
-            {
-                Console.WriteLine($"{AddCommand} - добавить игрока\n" +
-                    $"{RemoveCommand} - удалить игрока\n" +
-                    $"{СhangeBanCommand} - разбанить|забанить игрока\n" +
-                    $"{ShowCommand} - показать игроков\n" +
-                    $"{ExitCommand} - выйти");
-                Console.WriteLine("Введите команду:");
-
-                string userInput = Console.ReadLine();
-
-                switch (userInput)
+                foreach (CardSuit suit in Enum.GetValues(typeof(CardSuit)))
                 {
-                    case AddCommand:
-                        database.AddPlayer();
-                        break;
-
-                    case RemoveCommand:
-                        Console.Write("Введите ID игрока для удаления: ");
-
-                        if (int.TryParse(Console.ReadLine(), out int removeId))
-                            database.RemovePlayer(removeId);
-                        else
-                            Console.WriteLine("Неверный ввод.");
-
-                        break;
-                        
-                    case СhangeBanCommand:
-                        Console.Write("Введите ID игрока: ");
-
-                        if (int.TryParse(Console.ReadLine(), out int changeBanId))
-                            database.RemovePlayer(changeBanId);
-                        else
-                            Console.WriteLine("Неверный ввод.");
-
-                        break;
-
-                    case ShowCommand:
-                        database.ShowInfo();
-                        break;
-
-                    case ExitCommand:
-                        Console.WriteLine("Вы вышли.");
-                        isActive = false;
-                        break;
-
-                    default:
-                        Console.WriteLine("Неверный ввод.");
-                        break;
-                }
-            }
-        }
-    }
-
-    class Player
-    {
-        private int _id;
-        private string _nickname;
-        private int _level;
-        private bool _isBanned;
-
-        public Player(int id, string nickname, int level, bool isBanned)
-        {
-            _id = id;
-            _nickname = nickname;
-            _level = level;
-            _isBanned = isBanned;
-        }
-
-        public int Id { set { _id = value; }  get { return _id; } }
-        public string Nickname { set { _nickname = value; } get { return _nickname; } }
-        public int Level { set { _level = value; } get { return _level; } }
-        public bool IsBanned { set { _isBanned = value; } get { return _isBanned; } }
-
-        public static Player CreatePlayer(int id)
-        {
-            Console.WriteLine("Введите Nickname игрока");
-            string nickname = Console.ReadLine();
-            int level = 0;
-            bool isLevelCorrect = false;
-            bool isBanned = true;
-
-            while (isLevelCorrect == false)
-            {
-                Console.Write("Введите уровень угрока:");
-                string userInput = Console.ReadLine();
-
-                if (int.TryParse(userInput, out level) && level >= 0)
-                {
-                    isLevelCorrect = true;
-                }
-                else
-                {
-                    Console.WriteLine("Неверный ввод.");
-                }
-            }
-
-            return new Player(id, nickname, level, isBanned);
-        }
-
-        public void ShowInformation()
-        {
-            Console.WriteLine(" ID: " + Id);
-            Console.WriteLine(" NICKNAME: " + Nickname);
-            Console.WriteLine(" LEVEL: " + Level);
-            Console.WriteLine(" BAN: " + IsBanned);
-        }
-    }
-
-    class Database
-    {
-        private List<Player> _players;
-
-        public Database ()
-        {
-            _players = new List<Player>();
-        }
-        private int GetNewPlayerId()
-        {
-            int id = 0;
-            bool isIdCorrect = false;
-
-            while (isIdCorrect == false)
-            {
-                Console.Write("Введите ID игрока:");
-                string userInput = Console.ReadLine();
-
-                if (int.TryParse(userInput, out id) && id > 0)
-                {                    
-                    if(CheckIdExists(id) == false)
+                    foreach(CardValue value in Enum.GetValues(typeof(CardValue)))
                     {
-                        isIdCorrect = true;
+                        Card card = new Card(suit, value);
+                        cards.Add(card);                        
+                    }
+                }
+
+                return new Deck(cards);
+            }                        
+        }
+
+        private enum CardSuit
+        {
+            Hearts,    // Черви
+            Diamonds,  // Бубны
+            Clubs,     // Трефы
+            Spades     // Пики
+        }
+
+        private enum CardValue
+        {
+            Ace = 1,    // Туз
+            Two = 2,    // Два
+            Three = 3,  // Три
+            Four = 4,   // Четыре
+            Five = 5,   // Пять
+            Six = 6,    // Шесть
+            Seven = 7,  // Семь
+            Eight = 8,  // Восемь
+            Nine = 9,   // Девять
+            Ten = 10,   // Десятка
+            Jack = 11,  // Валет
+            Queen = 12, // Дама
+            King = 13   // Король
+        }
+
+        class Card
+        {
+            private CardSuit _suit;
+            private CardValue _value;
+
+            public Card(CardSuit suit, CardValue value)
+            {
+                _suit = suit;
+                _value = value;
+            }
+
+            public void Show()
+            {
+                Console.WriteLine($"{_value} - {_suit}");
+            }
+        }
+
+        class Deck
+        {
+            private List<Card> _cards;
+
+            public Deck(List<Card> cards)
+            {
+                _cards = cards;
+            }
+
+            public void Show()
+            {
+                foreach ( Card card in _cards )
+                    card.Show();
+            }
+
+            public void Shuffle()
+            {
+                Random random = new Random();
+
+                for (int i = 0; i < _cards.Count; i++)
+                {
+                    int randomIndex = random.Next(_cards.Count);
+                    Card buffer = _cards[i];
+                    _cards[i] = _cards[randomIndex];
+                    _cards[randomIndex] = buffer;
+                }
+            }
+
+            public List<Card> GetCards(int count)
+            {
+                List<Card> cards = _cards.GetRange(0, count);
+                _cards.RemoveRange(0, count);
+                return cards;
+            }
+
+            public int GetCount => _cards.Count;
+        }              
+
+        class Player
+        {
+            private List<Card> _cards;
+
+            public Player(string name)
+            {
+                Name = name;
+                _cards = new List<Card>();
+            }
+
+            public string Name { get; set; }
+
+            public void ShowCards()
+            {
+                foreach (Card card in _cards)
+                    card.Show();
+            }
+
+            public void TakeCards(List<Card> cards)
+            {
+                _cards.AddRange(cards);
+            }
+        }
+
+        class Dealer
+        {
+            private Deck _desk;
+            private Player _player;
+
+            public Dealer(Deck desk, Player player)
+            {
+                _desk = desk;
+                _player = player;
+            }
+
+            private int ReadCardsNumber()
+            {
+                bool isCorrectInput = false;
+                int cardsNumber = 0;
+
+                while (isCorrectInput == false)
+                {
+                    Console.Write("Ведите число карт для раздачи: ");
+                    string input = Console.ReadLine();
+
+                    if (IsCorrectCardsNumber(input,out cardsNumber))
+                    {
+                        isCorrectInput = true;
+                    }                    
+                }
+
+                return cardsNumber;
+            }
+
+            private bool IsCorrectCardsNumber(string input, out int cardsNumber)
+            {
+                if (int.TryParse(input, out cardsNumber) == true)
+                {
+                    if (cardsNumber > 0 && cardsNumber <= _desk.GetCount)
+                    {
+                        return true;
                     }
                     else
                     {
-                        Console.WriteLine("Неверный ввод.");
-                        continue;
-                    }                    
+                        return false;
+                    }
                 }
                 else
                 {
-                    Console.WriteLine("Неверный ввод.");
+                    return false;
                 }
             }
 
-            return id;
-        }
-
-        private bool CheckIdExists(int id)
-        {
-            bool isIdExists = false;
-
-            foreach (Player player in _players)
+            public void StartGame()
             {
-                if (player.Id == id)
-                {
-                    isIdExists = true;
-                    break;
-                }
-            }
-
-            return isIdExists;
-        }
-
-        public Player GetPlayerById(int id)
-        {
-            foreach (Player player in _players)
-            {
-                if (player.Id == id)
-                {
-                    return player;
-                }
-            }
-
-            return null;
-        }        
-
-        public void СhangePlayerBan(int id)
-        {
-            Player player = GetPlayerById(id);
-
-            if (player != null)
-                player.IsBanned = !player.IsBanned;
-            else
-                Console.WriteLine("Неверный ID.");
-        }
-
-        public void AddPlayer()
-        {
-            int id = GetNewPlayerId();
-            Player player = Player.CreatePlayer(id);
-            _players.Add(player);
-        }
-
-        public void RemovePlayer(int id)
-        {
-            Player player = GetPlayerById(id);
-
-            if (player != null)
-                _players.Remove(player);
-            else
-                Console.WriteLine("Неверный ID.");
-        }
-
-        public void ShowInfo()
-        {
-            foreach(Player player in _players)
-            {
-                player.ShowInformation();
+                _desk.Shuffle();
+                int desiredCards = ReadCardsNumber();
+                _player.TakeCards(_desk.GetCards(desiredCards));
+                Console.WriteLine(_player.Name);
+                _player.ShowCards();
             }
         }
     }
